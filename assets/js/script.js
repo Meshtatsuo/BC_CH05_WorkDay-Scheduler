3ed909f78ca781f4fbdabd6b024997d7976a9f4d
Dominic Misasi <meshtatsuo@gmail.com>
2022-01-23 19:39:06 -0700

    work on initial html and styling. some js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// FUNCTIONS
+
+// Generate each list item for the tasks
+
+const generateListItems = function () {
+  // Set classes for main divs beforehand. Makes it easy to tweak later
+  let liClasses =
+    "row item align-items-center text-light border-bottom border-1 border-light";
+  let timeSlotClasses =
+    "fill timeSlot p-2 col-1 text-dark text-end rounded-start";
+  let taskSlotClasses = "fill taskSlot p-2 col-10";
+  let saveSlotClasses = "fill saveSlot p-2 col-1 fill rounded-end";
+
+  // Create the needed elements for each list item
+  let listItemEl = $;
+};
+
+let listEl = $(".list");
+console.log(listEl);

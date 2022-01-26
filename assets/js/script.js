08c1e8afdaf74a2734f1344e597395dbb6ae74c3
MesH <meshtatsuo@gmail.com>
2022-01-25 18:32:04 -0700

    Time slot generation functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,69 @@
 // FUNCTIONS
+// variables to use to set workday schedule.
+// if time is left I will add an option to
+// allow you to set your own workday
+// (based on 24 hour schedule)
+var startingHour = 9;
+var endingHour = 17;
+var taskContainer = $(".list");
 
-// Generate each list item for the tasks
-
-const generateListItems = function () {
+// Generate each list item for the tasks based starting and ending hour
+const generateListItems = function (startingHour, EndingHour) {
   // Set classes for main divs beforehand. Makes it easy to tweak later
+
   let liClasses =
     "row item align-items-center text-light border-bottom border-1 border-light";
   let timeSlotClasses =
-    "fill timeSlot p-2 col-1 text-dark text-end rounded-start";
-  let taskSlotClasses = "fill taskSlot p-2 col-10";
-  let saveSlotClasses = "fill saveSlot p-2 col-1 fill rounded-end";
+    "fill timeSlot px-2 col-1 text-dark text-end rounded-start d-flex justify-content-end align-items-center";
+  id = "time";
+  let taskSlotClasses = "fill taskSlot px-2 col-10 d-flex align-items-center";
+  let saveSlotClasses =
+    "fill saveSlot px-2 col-1 fill rounded-end d-flex align-items-center justify-content-center";
 
-  // Create the needed elements for each list item
-  let listItemEl = $;
+  let numItems = endingHour - startingHour;
+
+  // checks if hour is in the afternoon. If true, flags it as afternoon and
+  // converts hour to the 12 hour clock
+  for (i = 0; i <= numItems; i++) {
+    let hour = i + startingHour;
+    let afternoon = false;
+    if (hour - 12 >= 0) {
+      if (hour - 12 === 0) {
+        afternoon = true;
+      } else {
+        afternoon = true;
+        hour = hour - 12;
+      }
+    } else {
+      afternoon = false;
+    }
+
+    // Create the needed elements for each list item
+    let listItemEl = $("<li>").addClass(liClasses);
+    let timeAreaEl = $("<div>").addClass(timeSlotClasses);
+    let taskAreaEl = $("<div>").addClass(taskSlotClasses + hour);
+    let saveAreaEl = $("<div>").addClass(saveSlotClasses);
+    // add ID of the task and save elements to match the time
+    // slot for future tracking
+    taskAreaEl.attr("id", "hour-" + hour);
+    saveAreaEl.attr("id", "hour-" + hour);
+    // Create text content for items
+    // Set time element, checking for before or after 12pm.
+    debugger;
+    if (!afternoon) {
+      timeAreaEl.html("<p>" + hour + "  AM </p>");
+    } else {
+      timeAreaEl.html("<p>" + hour + "  PM </p>");
+    }
+    saveAreaEl.html("<img src='assets/images/floppy-disk-solid.png' />");
+
+    listItemEl.append(timeAreaEl);
+    listItemEl.append(taskAreaEl);
+    listItemEl.append(saveAreaEl);
+    taskContainer.append(listItemEl);
+  }
 };
 
 let listEl = $(".list");
 console.log(listEl);
+generateListItems(startingHour, endingHour);
